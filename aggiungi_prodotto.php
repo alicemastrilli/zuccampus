7f5a486ba1ed681e049648fe015b6355a9cf434f
Alice Conti <alice.conti7@studio.unibo.it>
2022-01-12 23:17:54 +0100

    Create the page "aggiungi_prodotto"

---
+++ aggiungi_prodotto.php
@@ -0,0 +1,21 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["titolo"] = "Zuccampus - Registrazione";
+$templateParams["header"] = "header.php";
+$templateParams["footer"] = "footer.php";
+$templateParams["main"] = "aggiungi_prodotto.php";
+$templateParams["nome"] = $dbh->getNomeApp()[0]["nome_app"];
+$templateParams["info"] = $dbh->getAppInfo($templateParams["nome"])[0];
+$templateParams["links"] = $dbh->getLink($templateParams["nome"]);
+
+$templateParams["aziende_agricole"] = $dbh->getAziendaAgricolaInfo();
+/*
+$templateParams["articolicasuali"] = $dbh->getRandomPosts(2);
+$templateParams["categorie"] = $dbh->getCategories();
+
+$templateParams["articoli"] = $dbh->getPosts(2);
+*/
+
+require("template/homePage.php");
+?>
---
+++ template/aggiungi_prodotto.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <link rel="stylesheet" type="text/css" href="./css/venditore.css" /> 
+    </head>
+    <section>
+        <div class="row text-center">
+        <h3 class=" px-2">Aggiungi un nuovo prodotto al tuo negozio</h3>
+            
+        </div>
+        <article class="rounded mx-2">
+            <div class="col-12 p-3 text-center ">
+                <img src="./icons/utente_generico.jpg" alt="foto profilo default"/>
+                <div class="pb-1 text-center">
+                    <form  action="#" method="get">
+                        <button class="rounded p-1" type="button" name="Inserisci imamgine prodotto" >Inserisci immagine prodotto</button>
+                    </form>
+                </div>
+            </div>
+            <div class="mb-3 mt-3">
+                <!--gestire per ogni input l'inserimento di una nuova riga nel database-->
+                <label for="nome zucca" class="form-label px-2">Nome zucca:</label><br>
+                <div class="mx-2 pb-3">
+                   <input class="form-control" type="text" id="nome" name="nome zucca" placeholder="Inserisci nome zucca">
+                </div>
+                <!--Categoria far scegliere da una tendina-->
+                <label for="categoria" class="form-label px-2 ">Categoria:</label><br>
+                <div class="mx-2 pb-3">
+                   <input class="form-control" type="text" id="Categoria" name="Categoria" placeholder="Inserisci categoria">
+                </div>
+                <label for="descrizione_zucca" class="form-label px-2">Descrizione:</label><br>
+                <div class="mx-2 pb-3">
+                    <!--controllare il placeholder della textarea-->
+                   <textarea class="form-control" rows="5" id="descrizione_zucca" 
+                   name="descrizione_azienda_agricola" placeholder="Inserisci descrizione azienda agricola"> </textarea>
+                </div>
+              </div>
+        </article>
+        
+        <div class="pb-3 text-center">
+        <form  action="#" method="get">
+            <button class="rounded p-2" type="button" name="Salva" >Salva prodotto</button>
+        </form>
+        </div>
+    </section>
