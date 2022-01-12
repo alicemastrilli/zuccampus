67017cad42f25adcdf6e659caa8f6d41109ab38c
Alice Conti <alice.conti7@studio.unibo.it>
2022-01-12 15:24:06 +0100

    Create the "register" page

---
+++ registrati.php
@@ -0,0 +1,21 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["titolo"] = "Zuccampus- Aziende Agricole";
+$templateParams["header"] = "header.php";
+$templateParams["footer"] = "footer.php";
+$templateParams["main"] = "registrati.php";
+$templateParams["nome"] = $dbh->getNomeApp()[0]["nome_app"];
+$templateParams["info"] = $dbh->getAppInfo($templateParams["nome"])[0];
+$templateParams["links"] = $dbh->getLink($templateParams["nome"]);
+
+$templateParams["aziende_agricole"] = $dbh->getAziendaAgricolaInfo();
+/*
+$templateParams["articolicasuali"] = $dbh->getRandomPosts(2);
+$templateParams["categorie"] = $dbh->getCategories();
+
+$templateParams["articoli"] = $dbh->getPosts(2);
+*/
+
+require("template/homePage.php");
+?>
---
+++ template/registrati.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <link rel="stylesheet" type="text/css" href="./css/aziende_agricole.css" /> 
+
+    </head>
+        <table class="table table-striped">
+            <tbody>
+                <tr>
+                <td>
+                    <div>
+                        <div> 
+                            <form  action="#" method="get">
+                            <button class="rounded">Sono un cliente</button>
+                            </form>
+                        </div> 
+                    </div> 
+                </td>
+                <td>
+                <div class="row">
+                        <div class="col-6 text-center"> 
+                            <form  action="#" method="get">
+                            <button class="rounded">Sono un venditore</button>
+                            </form>
+                        </div> 
+                    </div>
+                </td>
+                </tr>
+            </tbody>
+        </table>
