f9ddf2432584ea3ec7e086d2e658f3b70b1d2cc0
Alice <alice.mastrilli@studio.unibo.it>
2022-01-13 11:57:40 +0100

    gestito caso casella di posta buota

--- casella_messaggi.php
+++ casella_messaggi.php
@@ -6,6 +6,7 @@ $templateParams["header"] = "header.php";
 $templateParams["footer"] = "footer.php";
 
 $templateParams["messaggi"] = $dbh->getMessaggi($_SESSION["username"]);
+var_dump($templateParams["messaggi"]);
  $templateParams["main"] = "messaggio.php";
 $templateParams["nome"] = $dbh->getNomeApp()[0]["nome_app"];
 $templateParams["info"] = $dbh->getAppInfo($templateParams["nome"])[0];
--- template/messaggio.php
+++ template/messaggio.php
@@ -3,11 +3,19 @@
     <head>
         <link rel="stylesheet" href="./css/messaggio.css" > 
     </head>
-<p class="text-center">12 gennaio 2022</p>
+    <?php if (count($templateParams["messaggi"])==0):?>
+        <p >La casella di posta è vuota</p>
+    <?php else :?>
+        <?php foreach($templateParams["messaggi"] as $messaggio):?>
+<p class="text-center"><?php echo $messaggio["data"]?></p>
     <div class="text-center mx-3">
     <article class=" text-center mb-2 ">
         <span class="p-2">Gentile Mario, la sua azienda agricola ha ricevuto un  nuovo ordine: 2 x zucca piccola. <a href="#">Clicchi qui</a>  <span > per visualizzare l’ordine.
            <br> <span class="px-3 d-flex flex-row-reverse "><small>16:30</small> </span>
     </article> 
-</div>  
-  <hr />
+    </div>  
+  <hr/>
+
+  <?php endforeach;?>
+  <?php endif;?>
+  </html>
