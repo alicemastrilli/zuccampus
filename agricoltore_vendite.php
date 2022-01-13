95871fcd92c6225eddf0dae069738d9ae56651b0
Alice <alice.mastrilli@studio.unibo.it>
2022-01-13 22:17:27 +0100

    sistemata visualizzazione grafico tramite js

--- agricoltore_vendite.php
+++ agricoltore_vendite.php
@@ -5,6 +5,7 @@ $templateParams["titolo"] = "Zuccampus-Vendite Agricoltore";
 $templateParams["header"] = "header.php";
 $templateParams["footer"] = "footer.php";
 $templateParams["main_agr"] = "agricoltore_vendite.php";
+$templateParams["js"] = JS_DIR."chart.js";
 $templateParams["main"] = "agricoltore_base.php";
 $templateParams["nome"] = $dbh->getNomeApp()[0]["nome_app"];
 $templateParams["info"] = $dbh->getAppInfo($templateParams["nome"])[0];
--- bootstrap.php
+++ bootstrap.php
@@ -4,4 +4,6 @@ require_once("db/database.php");
 require_once("utils/functions.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "zuccampus", 3306);
 define("UPLOAD_DIR", "./icons/");
+define("JS_DIR", "./js/");
+define("CSS_DIR", "./css/");
 ?>
