c30b8dd170f9a4a31423f3d5d499ae03cae54f73
Alice <alice.mastrilli@studio.unibo.it>
2022-01-22 11:42:27 +0100

    modificato insert_data per gestire cliente/agricoltore

--- db/insert_data.sql
+++ db/insert_data.sql
@@ -12,15 +12,15 @@ INSERT INTO `azienda_agricola` (`nome_azienda`, `via`, `numero_civico`, `cap`, `
 ('Gigi e le sue zucche', 'via dei Fiori', '45', '40013', 'Gigi ha un unico grande amore: le sue zucche!\r\n\Talmente grande che fa ingelosire la moglie, e tutti i vicini!\r\nNel gusto troverai la sua passione'),
 ('La gioia di Marta', 'via del Campo', '14', '40013', 'Marta cura le sue zucche come se fossero sue figlie!\r\nAssapora l''amore!');
 
-INSERT INTO `utente` (`immagine`, `num_telefono`, `email`, `username`, `password`, `nome`, `cognome`) VALUES 
-('mario.png', '92746', 'mario@gmail.com', 'MarioRossi', 'mario_71', 'Mario', 'Rossi'),
-('marta.jpeg', '92345', 'marta@gmail.com', 'MartaGioia', 'Marta_81', 'Marta', 'Creti'),
-('gigi.jpeg', '92476', 'gigi@gmail.com', 'Gigiiii', 'Gigetto', 'Gigi', 'Carlino');
+INSERT INTO `utente` (`immagine`, `num_telefono`, `email`, `username`, `password`, `nome`, `cognome`,`CLIENTE`,`AGRICOLTORE`) VALUES 
+('mario.png', '92746', 'mario@gmail.com', 'MarioRossi', 'mario_71', 'Mario', 'Rossi',0,1),
+('marta.jpeg', '92345', 'marta@gmail.com', 'MartaGioia', 'Marta_81', 'Marta', 'Creti',0,1),
+('gigi.jpeg', '92476', 'gigi@gmail.com', 'Gigiiii', 'Gigetto', 'Gigi', 'Carlino',0,1);
 
-INSERT INTO `utente` (`immagine`, `num_telefono`, `email`, `username`, `password`, `nome`, `cognome`) VALUES
-(NULL, '3395872', 'alessia.gentili@studio.unibo.it', 'GentiAle', 'GentiAle', 'Alessia', 'Gentili'),
-('fotoprofilo.jpeg', '3357263485', 'francesco.verdi@studio.unibo.it', 'Verdi', 'FraVe', 'Francesco', 'Verdi'),
-('', '3319558', 'f.carletti@unibo.it', 'ProfCarletti', 'SonoBello', 'Federico', 'Carletti');
+INSERT INTO `utente` (`immagine`, `num_telefono`, `email`, `username`, `password`, `nome`, `cognome`,`CLIENTE`,`AGRICOLTORE`) VALUES
+(NULL, '3395872', 'alessia.gentili@studio.unibo.it', 'GentiAle', 'GentiAle', 'Alessia', 'Gentili',1,0),
+('fotoprofilo.jpeg', '3357263485', 'francesco.verdi@studio.unibo.it', 'Verdi', 'FraVe', 'Francesco', 'Verdi',1,0),
+('', '3319558', 'f.carletti@unibo.it', 'ProfCarletti', 'SonoBello', 'Federico', 'Carletti',1,0);
 
 INSERT INTO `cliente` (`username`, `matricola`) VALUES 
 ('GentiAle', '910004625'),
