6f61835aea5e9c7c7820051ac12223dc7ee79ebd
Alice Conti <alice.conti7@studio.unibo.it>
2022-02-02 23:05:26 +0100

    Corretto nome zucca in salva_pagamento
    disponibilita ancora da correggere

--- salva_pagamento.php
+++ salva_pagamento.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Europe/Berlin');
 $t = time();
 $username = $_SESSION["username"];
 $data_ordine = date("Y-m-d");;
-$ora = date("h:i:sa", $t);
+$ora = date("H:i:sa", $t);
 $via = "Via dell'Università";
 $numero_civico = "50";
 $cap = "40013";
@@ -18,14 +18,15 @@ $msg = $dbh-> insertNewOrdine($username, $data_ordine, $ora, $via, $numero_civic
 //decremento la quantita in zucca
 if($msg){
     foreach($_SESSION['product'] as $prodotto){
-        $nome_zucca = $prodotto["id"][0];
-        $quantity = $prodotto["quantita"];
-        $templateParams["zucca"] = $dbh -> getZuccaByName($nome_zucca);
+        $nome_zucca = $prodotto["nome"];
+        $quantity = $prodotto["quantita"][0];
+        $templateParams["zucca"] = $dbh -> getZuccaByName($nome_zucca)[0];
         $zucca = $templateParams["zucca"];
-        $msg = $dbh -> updateZucca($zucca["immagine"], $zucca["prezzo"], $zucca["peso"], $zucca["disponibilita"], $zucca["descrizione"], $zucca["nome_azienda"], $zucca["nome_zucca"], $zucca["tipo"]);
+        $disponibilita = bcsub($zucca["quantita"], $quantity);
+        $msg = $dbh -> updateZucca($zucca["immagine"], $zucca["prezzo"], $zucca["peso"], $disponibilita, $zucca["descrizione"], $zucca["nome_azienda"], $zucca["nome_zucca"], $zucca["tipo"]);
     }
 }
 
-header("location: casella_messaggi.php?formmsg=".$msg);
+header("location: casella_messaggi.php?formmsg=".$zucca["quantita"]);
 
 ?>
