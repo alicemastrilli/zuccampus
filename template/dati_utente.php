c72bb239e6409048fde13416ce5c07d8565b8aea
Alice Conti <alice.conti7@studio.unibo.it>
2022-02-08 23:25:04 +0100

    aggiunto header e footer a homepage

--- template/dati_utente.php
+++ template/dati_utente.php
@@ -16,19 +16,19 @@ if(isset($_GET["id"]) || $_SESSION["agricoltore"] == 1){
         <img class="fotoprofilo" src="<?php echo UPLOAD_DIR.getImageOfUser($templateParams["utente"]["immagine"]);?>" class=" rounded-circle border border-2 border-dark" 
         alt="<?php echo $templateParams["utente"]["nome"]?>"/>
         </div>
-        </div>
+      </div>
         <h3 class="text-center"><?php echo $templateParams["utente"]["nome"]; ?> <?php echo $templateParams["utente"]["cognome"]; ?></h3>
         <h5 class="text-center"><?php echo $templateParams["utente"]["email"]; ?></h5>
-        <article class="rounded mx-2">
+        <article class=" mx-2">
             <h3 class="pt-2 px-2">Informazioni di base</h3>
-            <div class="mb-3 mt-3">
+            <div >
                 <label for="nome" class="form-label px-2">Nome:</label><br>
-                <div class="mx-2 pb-3">
+                <div class="mx-2">
                    <input  type="text" class="input-box" id="nome" value="<?php echo $templateParams["utente"]["nome"]; ?>" name="nome" readonly>
                 </div>
                 <label for="cognome" class="form-label px-2 ">Cognome:</label><br>
                 <div class="mx-2 pb-3">
-                   <input  type="text" class="form-control " id="cognome" value="<?php echo $templateParams["utente"]["cognome"]; ?>" name="cognome" readonly>
+                   <input  type="text" class="input-box " id="cognome" value="<?php echo $templateParams["utente"]["cognome"]; ?>" name="cognome" readonly>
                 </div>
             </div>
         </article>
@@ -39,25 +39,25 @@ if(isset($_GET["id"]) || $_SESSION["agricoltore"] == 1){
             <div class="mb-3 mt-3">
             <label for="azienda_agricola" class="form-label px-2">Azienda agricola:</label><br>
                 <div class="mx-2 pb-3">
-                   <input  type="text" class="form-control " id="azienda_agricola" value="<?php echo $azienda["nome_azienda"]; ?>"
+                   <input  type="text" class="input-box" id="azienda_agricola" value="<?php echo $azienda["nome_azienda"]; ?>"
                     name="azienda_agricola" readonly>
                 </div>
                 <label for="descrizione_azienda_agricola" class="form-label px-2">Descrizione Azienda agricola:</label><br>
                 <div class="mx-2 pb-3">
-                   <textarea class="form-control" rows="5" id="descrizione_azienda_agricola" 
+                   <textarea class="input-box" rows="5" id="descrizione_azienda_agricola" 
                    name="descrizione_azienda_agricola" readonly><?php echo $azienda["descrizione"]; ?> </textarea>
                 </div>
                 <label for="indirizzo" class="form-label px-2">Indirizzo:</label><br>
                 <div class="mx-2 pb-3">
-                   <input  type="text" class="form-control " id="indirizzo" value="<?php echo $azienda["via"]; ?> <?php echo $azienda["numero_civico"]; ?>" name="indirizzo" readonly>
+                   <input  type="text" class="input-box" id="indirizzo" value="<?php echo $azienda["via"]; ?> <?php echo $azienda["numero_civico"]; ?>" name="indirizzo" readonly>
                 </div>
                 <label for="città" class="form-label px-2">Città:</label><br>
                 <div class="mx-2 pb-3">
-                   <input  type="text" class="form-control " id="città" value="<?php echo $azienda["citta"]; ?>" name="città" readonly>
+                   <input  type="text" class="input-box" id="città" value="<?php echo $azienda["citta"]; ?>" name="città" readonly>
                 </div>
                 <label for="cap" class="form-label px-2">Cap:</label><br>
                 <div class="mx-2 pb-3">
-                   <input  type="number" class="form-control " id="cap" value="<?php echo $azienda["cap"]; ?>" name="cap" readonly>
+                   <input  type="number" class="input-box" id="cap" value="<?php echo $azienda["cap"]; ?>" name="cap" readonly>
                 </div>
               </div>
         </article>
@@ -67,11 +67,11 @@ if(isset($_GET["id"]) || $_SESSION["agricoltore"] == 1){
             <div class="mb-3 mt-3">
                 <label for="email" class="form-label px-2">Email:</label><br>
                 <div class="mx-2 pb-3">
-                   <input  type="email" class="form-control " id="email" value="<?php echo $templateParams["utente"]["email"]; ?>" name="email" readonly>
+                   <input  type="email" class="input-box" id="email" value="<?php echo $templateParams["utente"]["email"]; ?>" name="email" readonly>
                 </div>
                 <label for="num_telefono" class="form-label px-2">Numero telefonico:</label><br>
                 <div class="mx-2 pb-3">
-                   <input  type="number" class="form-control " id="num_telefono" value="<?php echo $templateParams["utente"]["num_telefono"]; ?>" name="numero_telefono" readonly>
+                   <input  type="number" class="input-box" id="num_telefono" value="<?php echo $templateParams["utente"]["num_telefono"]; ?>" name="numero_telefono" readonly>
                 </div>
                 
               </div>
