25f99b1e4cf1565cc0930251e3e0883d7672d96c
Alice Conti <alice.conti7@studio.unibo.it>
2022-02-09 23:13:34 +0100

    Sistemato stile registrati

--- template/registrati.php
+++ template/registrati.php
@@ -6,20 +6,20 @@
 
     <div class="container-fluid">
         <div class="row m-2">
-            <div class="col-sm-0 text-center">
+            <div class="profilo col-sm-0 text-center">
                 <img class="img-fluid my-4" src="./icons/icona_login.png" alt="login cliente"/>
                 <form action="gestisci_registrazione.php?action=1" method="post">
-                    <button class="rounded p-4"  name="cliente">Sono un cliente</button>
+                    <button class="rounded"  name="cliente">Sono un cliente</button>
                     <input type="hidden" name="cliente" value="cliente" />
                 </form>
                
             </div>
         </div>
         <div class="row m-2">
-            <div class="col-sm-0 text-center">
+            <div class="profilo col-sm-0 text-center">
                 <img class="img-fluid my-4" src="./icons/icona_login_agricoltore.png" alt="login agricoltore"/>
                 <form action="gestisci_registrazione.php?action=1" method="post">
-                    <button class="rounded p-4" name="agricoltore">Sono un agricoltore</button>
+                    <button class="rounded" name="agricoltore">Sono un agricoltore</button>
                 <input type="hidden" name="agricoltore" value="agricoltore" />    
             </form>
             </div>
