ca38779e8ad93871676d0edeb5761cb3adde78b2
Elisa Barberini <elisabarberini0025@gmail.com>
2022-02-10 23:02:11 +0100

    sistemazione dettagli

--- template/info_prodotti.php
+++ template/info_prodotti.php
@@ -137,6 +137,6 @@ if(isset($_POST['submit'])){
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="./js/elisa.js"></script>
+    <script src="./js/prodotti.js"></script>
     <script src="./js/window_functions.js"></script>
 </html>
